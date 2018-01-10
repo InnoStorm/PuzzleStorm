b433fd74a5cf0f07e7cb8c60f4db961c46b8dca4
Nikola Savic <nikolaaasavic@live.com>
2018-01-10 02:34:28 +0100

    Dodato pokretanje Dispose-a prilikom klika na X dugme

--- Projekat/PuzzleStorm/Server/Program.cs
+++ Projekat/PuzzleStorm/Server/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Mime;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using DataLayer.Core.Domain;
@@ -15,6 +17,38 @@ using EasyNetQ;
 namespace Server {
     class Program
     {
+        #region CleanExit
+        [DllImport("Kernel32")]
+        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+
+        private delegate bool EventHandler(CtrlType sig);
+        static EventHandler _handler;
+
+        enum CtrlType {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+        }
+
+        private static bool Handler(CtrlType sig) {
+            switch (sig) {
+                case CtrlType.CTRL_C_EVENT:
+                case CtrlType.CTRL_LOGOFF_EVENT:
+                case CtrlType.CTRL_SHUTDOWN_EVENT:
+                case CtrlType.CTRL_CLOSE_EVENT:
+                    _communicatorBus.Dispose();
+                    return false;
+                default:
+                    return false;
+            }
+        }
+
+        
+        #endregion
+
+
         private static BlockingCollection<UserAuthWorker> _workersPool;
         private static IBus _communicatorBus;
 
@@ -53,6 +87,10 @@ namespace Server {
 
         static void Main(string[] args) 
         {
+            _handler += new EventHandler(Handler);
+            SetConsoleCtrlHandler(_handler, true);
+
+
             Console.WriteLine("Starting up server...");
             InitServer();
 
