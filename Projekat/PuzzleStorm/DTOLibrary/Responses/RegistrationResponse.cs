a317b23f8c15c211cdd5f5ab75a30b5be7f0a5cc
Nikola Savic <nikolaaasavic@live.com>
2018-01-10 01:26:51 +0100

    Dodat novi projekat DTOLibrary

---
+++ Projekat/PuzzleStorm/DTOLibrary/Responses/RegistrationResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTOLibrary.Enums;
+
+namespace DTOLibrary.Responses {
+    public class RegistrationResponse {
+        public string Username { get; set; }
+        public OperationStatus Status { get; set; }
+        public string Details { get; set; }
+    }
+}
