c60ec6e901269f04eee8c074c67d145eb18ff16f
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2018-01-15 16:12:33 +0100

    Dodat projekat ServerLobby

---
+++ Projekat/PuzzleStorm/ServerLobby/ServerLobby.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server;
+
+namespace ServerLobby
+{
+    class ServerLobby : StormServer
+    {
+        static void Main(string[] args)
+        {
+            ServerInstance = new ServerLobby();
+            ServerInstance.Start();
+        }
+
+        protected override void StartupInit()
+        {
+            InitWorkerPool();
+            BindWorkerMethods();
+        }
+
+        private void InitWorkerPool()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BindWorkerMethods()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        protected override void ShutdownCleanUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Projekat/PuzzleStorm/ServerLobby/Workers/LobbyWorker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server.Workers;
+
+namespace ServerLobby.Workers
+{
+    class LobbyWorker : Worker
+    {
+    }
+}
