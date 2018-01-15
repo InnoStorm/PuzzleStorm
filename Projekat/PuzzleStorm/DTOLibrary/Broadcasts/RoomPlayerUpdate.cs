2f21087af3b52235356132d7fff6b9261995e5a0
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2018-01-15 15:41:54 +0100

    Specifirani nazivi RabbitMQ redova

--- Projekat/PuzzleStorm/DTOLibrary/Broadcasts/RoomPlayerUpdate.cs
+++ Projekat/PuzzleStorm/DTOLibrary/Broadcasts/RoomPlayerUpdate.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using DTOLibrary.Enums;
 using DTOLibrary.Requests;
 using DTOLibrary.SubDTOs;
+using EasyNetQ;
 
 namespace DTOLibrary.Broadcasts
 {
+    [Queue(queueName: "RoomPlayerUpdateQueue", ExchangeName = "InRoomUpdatesExchange")]
     public class RoomPlayerUpdate : PostLoginRequest
     {
         public RoomPlayerUpdateType UpdateType { get; set; }
--- Projekat/PuzzleStorm/DTOLibrary/Broadcasts/RoomsStateUpdate.cs
+++ Projekat/PuzzleStorm/DTOLibrary/Broadcasts/RoomsStateUpdate.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DTOLibrary.Enums;
+using DTOLibrary.SubDTOs;
+using EasyNetQ;
 
 namespace DTOLibrary.Broadcasts
 {
+    [Queue(queueName: "RoomsStateUpdateQueue", ExchangeName = "RoomsChangesExchange")]
     public class RoomsStateUpdate : BroadcastMessage
     {
         public RoomUpdateType UpdateType { get; set; }
 
-        public int CreatorId { get; set; }
+        public Player Creator { get; set; }
         public int RoomId { get; set; }
         public PuzzleDifficulty Level { get; set; }
         public int NumberOfRounds { get; set; }
