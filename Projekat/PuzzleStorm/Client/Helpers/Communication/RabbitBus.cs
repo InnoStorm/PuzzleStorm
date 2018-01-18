5bffa5d926fe36f22b625cb2fd1e191e426ebb0e
Nikola Savic <nikolaaasavic@live.com>
2018-01-18 19:14:56 +0100

    Otvaranje rabbitbus-a u singlton klasi

---
+++ Projekat/PuzzleStorm/Client/Helpers/Communication/RabbitBus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EasyNetQ;
+
+namespace Client {
+
+    public sealed class RabbitBus
+    {
+        private static readonly Lazy<RabbitBus> BusInstance
+                            = new Lazy<RabbitBus>(() => new RabbitBus("amqp://ygunknwy:pAncRrH8Gxk3ULDyy-Wju7NIqdBThwCJ@sheep.rmq.cloudamqp.com/ygunknwy"));
+        public IBus Bus { get; set; } 
+
+        public static RabbitBus Instance => BusInstance.Value;
+
+        private RabbitBus() {
+        }
+
+        private RabbitBus(string bus)
+        {
+            this.Bus = RabbitHutch.CreateBus(bus);
+        }
+    }
+}
