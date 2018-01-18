1799b9c309788d2059fc192e95adedd0403d985f
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2018-01-18 21:15:12 +0100

    Dodati DTO za AllRoomsRequests

---
+++ Projekat/PuzzleStorm/DTOLibrary/Responses/GetAllRoomsResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTOLibrary.SubDTOs;
+
+namespace DTOLibrary.Responses
+{
+    public class GetAllRoomsResponse : Response
+    {
+        public List<RoomInfo> List { get; set; }
+    }
+}
---
+++ Projekat/PuzzleStorm/DTOLibrary/SubDTOs/RoomInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTOLibrary.Enums;
+
+namespace DTOLibrary.SubDTOs
+{
+    public class RoomInfo
+    {
+        public int RoomId { get; set; }
+        public int CreatorUsername { get; set; }
+        public PuzzleDifficulty Level { get; set; }
+        public int MaxPlayers { get; set; }
+        public int NumberOfRounds { set; get; }
+    }
+}
