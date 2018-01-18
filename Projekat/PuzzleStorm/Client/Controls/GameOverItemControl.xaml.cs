57cee649756182ec9268acc9d2eece16735d7113
Nikola Savic <nikolaaasavic@live.com>
2018-01-18 19:26:03 +0100

    Game over stranica

---
+++ Projekat/PuzzleStorm/Client/Controls/GameOverItemControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Client {
+    /// <summary>
+    /// Interaction logic for GameOverItemControl.xaml
+    /// </summary>
+    public partial class GameOverItemControl : UserControl {
+        public GameOverItemControl() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Projekat/PuzzleStorm/Client/Pages/GameOverPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Client {
+    /// <summary>
+    /// Interaction logic for GameOverPage.xaml
+    /// </summary>
+    public partial class GameOverPage : BasePage<GameOverPageViewModel> {
+        public GameOverPage() {
+            InitializeComponent();
+        }
+    }
+}
