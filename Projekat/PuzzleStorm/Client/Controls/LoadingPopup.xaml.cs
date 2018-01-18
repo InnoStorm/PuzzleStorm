3379541e2790938904d542a77a80b2ce67dba5e2
Nikola Savic <nikolaaasavic@live.com>
2018-01-18 19:23:36 +0100

    Kontrole za popup poruke

---
+++ Projekat/PuzzleStorm/Client/Controls/LoadingPopup.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Client {
+    /// <summary>
+    /// Interaction logic for LoadingPopup.xaml
+    /// </summary>
+    public partial class LoadingPopup : UserControl {
+        public LoadingPopup() {
+            InitializeComponent();
+        }
+    }
+}
