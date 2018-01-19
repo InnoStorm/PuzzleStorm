68ebde02690f014024944d7cf35a7f6212b86f2f
Nikola Savic <nikolaaasavic@live.com>
2018-01-19 04:25:44 +0100

    List rooms instanca

---
+++ Projekat/PuzzleStorm/Client/DataModels/ListRooms.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Client {
+
+    public sealed class ListRooms {
+
+        private static ListRooms instance = null;
+
+        public List<RoomsPropsViewModel> RoomsItemsList { get; set; }
+
+        private ListRooms() {
+            RoomsItemsList = new List<RoomsPropsViewModel>();
+        }
+
+        public static ListRooms Instance {
+            get {
+                if (instance == null) {
+                    instance = new ListRooms();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
