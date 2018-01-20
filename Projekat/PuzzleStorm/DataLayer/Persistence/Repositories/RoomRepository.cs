033712ecfddcb50194dbd7e738ecadc42aa01683
Marija Stojković <stojkovicmarija95@gmail.com>
2018-01-20 01:01:01 +0100

    Dodat RoomRepository

---
+++ Projekat/PuzzleStorm/DataLayer/Persistence/Repositories/RoomRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataLayer.Core.Domain;
+using DataLayer.Core.Repositories;
+using System.Data.Entity;
+
+namespace DataLayer.Persistence.Repositories
+{
+    public class RoomRepository : Repository<Room>, IRoomRepository
+    {
+        public RoomRepository(StormContext context) : base(context)
+        {
+
+        }
+        
+        public StormContext StormContext => Context as StormContext;
+    }
+}
