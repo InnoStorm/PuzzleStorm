a6766a89e1348acc44c393bb285ae3b7f029e154
Nikola Savic <nikolaaasavic@live.com>
2018-02-10 11:00:32 +0100

    Promenjen tip projekta .NET Standrad -> .NET Framework

---
+++ Projekat/StormCommonData/Enums/RoomState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StormCommonData.Enums
+{
+    public enum RoomState
+    {
+        Available,
+        Full,
+        Playing,
+        Finished,
+        Error
+    }
+}
