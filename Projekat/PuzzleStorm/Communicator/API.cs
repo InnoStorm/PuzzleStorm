5da3158f6e7e31ccbd5b0707175ef6d3b5375154
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2018-02-16 16:36:03 +0100

    Dodati async/sync parovi funkcija

--- Projekat/PuzzleStorm/Communicator/API.cs
+++ Projekat/PuzzleStorm/Communicator/API.cs
@@ -41,7 +41,25 @@ namespace Communicator
 
         #region Requests
 
-        //Generic wrapper
+        //Generic wrappers
+        private TResponse Request<TRequest, TResponse>(TRequest request)
+            where TRequest : Request, new()
+            where TResponse : Response, new()
+        {
+            try
+            {
+                return _bus.Request<TRequest, TResponse>(request);
+            }
+            catch (Exception ex)
+            {
+                return new TResponse()
+                {
+                    Status = OperationStatus.Failed,
+                    Details = "Timeout!" + Environment.NewLine + StormUtils.FlattenException(ex)
+                };
+            }
+        }
+
         private async Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
             where TRequest : Request, new()
             where TResponse : Response, new()
@@ -60,91 +78,143 @@ namespace Communicator
             }
         }
 
+        
         /// <summary>
         /// Login
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
-        public async Task<LoginResponse> LoginAsync(LoginRequest request)
-        {
-            return await RequestAsync<LoginRequest, LoginResponse>(request);
-        }
+        public LoginResponse Login(LoginRequest request)
+            => Request<LoginRequest, LoginResponse>(request);
+        
+        /// <summary>
+        /// Login
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public async Task<LoginResponse> LoginAsync(LoginRequest request) 
+            => await RequestAsync<LoginRequest, LoginResponse>(request);
 
+
+        
         /// <summary>
         /// Create new account
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
-        public async Task<RegistrationResponse> RegisterAsync(RegistrationRequest request)
-        {
-            return await RequestAsync<RegistrationRequest, RegistrationResponse>(request);
-        }
-
+        public RegistrationResponse Register(RegistrationRequest request)
+            => Request<RegistrationRequest, RegistrationResponse>(request);
+        
         /// <summary>
-        /// Create new room
+        /// Create new account
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
-        public async Task<SignOutResponse> SignOutAsync(SignOutRequest request)
-        {
-            return await RequestAsync<SignOutRequest, SignOutResponse>(request);
-        }
+        public async Task<RegistrationResponse> RegisterAsync(RegistrationRequest request) 
+            => await RequestAsync<RegistrationRequest, RegistrationResponse>(request);
+
+
+        
+        /// <summary>
+        /// SignOut
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public Task<SignOutResponse> SignOut(SignOutRequest request)
+            => RequestAsync<SignOutRequest, SignOutResponse>(request);
+
+        /// <summary>
+        /// SignOutAsync
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public async Task<SignOutResponse> SignOutAsync(SignOutRequest request) 
+            => await RequestAsync<SignOutRequest, SignOutResponse>(request);
+
+
+
 
 
         //Room
+        public CreateRoomResponse CreateRoom(CreateRoomRequest request)
+            => Request<CreateRoomRequest, CreateRoomResponse>(request);
+
         public async Task<CreateRoomResponse> CreateRoomAsync(CreateRoomRequest request)
-        {
-            return await RequestAsync<CreateRoomRequest, CreateRoomResponse>(request);
-        }
+            => await RequestAsync<CreateRoomRequest, CreateRoomResponse>(request);
+
+
+
+        public CancelRoomResponse CancelRoom(CancelRoomRequest request)
+            => Request<CancelRoomRequest, CancelRoomResponse>(request);
 
         public async Task<CancelRoomResponse> CancelRoomAsync(CancelRoomRequest request)
-        {
-            return await RequestAsync<CancelRoomRequest, CancelRoomResponse>(request);
-        }
+            => await RequestAsync<CancelRoomRequest, CancelRoomResponse>(request);
+
+
+
+        public GetAllRoomsResponse GetAllRooms(GetAllRoomsRequest request)
+            => Request<GetAllRoomsRequest, GetAllRoomsResponse>(request);
 
         public async Task<GetAllRoomsResponse> GetAllRoomsAsync(GetAllRoomsRequest request)
-        {
-            return await RequestAsync<GetAllRoomsRequest, GetAllRoomsResponse>(request);
-        }
+            => await RequestAsync<GetAllRoomsRequest, GetAllRoomsResponse>(request);
+
+
+
+        public RoomCurrentStateResponse GetRoomCurrentState(RoomCurrentStateRequest request)
+            => Request<RoomCurrentStateRequest, RoomCurrentStateResponse>(request);
 
         public async Task<RoomCurrentStateResponse> GetRoomCurrentStateAsync(RoomCurrentStateRequest request)
-        {
-            return await RequestAsync <RoomCurrentStateRequest, RoomCurrentStateResponse>(request);
-        }
+            => await RequestAsync<RoomCurrentStateRequest, RoomCurrentStateResponse>(request);
+
+
+
+        public ChangeRoomPropertiesResponse ChangeRoomProperties(ChangeRoomPropertiesRequest request)
+            => Request<ChangeRoomPropertiesRequest, ChangeRoomPropertiesResponse>(request);
 
         public async Task<ChangeRoomPropertiesResponse> ChangeRoomPropertiesAsync(ChangeRoomPropertiesRequest request)
-        {
-            return await RequestAsync <ChangeRoomPropertiesRequest, ChangeRoomPropertiesResponse> (request);
-        }
+            => await RequestAsync<ChangeRoomPropertiesRequest, ChangeRoomPropertiesResponse>(request);
+
 
 
         //InRoom
+        public ChangeStatusResponse ChangeStatus(ChangeStatusRequest request)
+            => Request<ChangeStatusRequest, ChangeStatusResponse>(request);
+
         public async Task<ChangeStatusResponse> ChangeStatusAsync(ChangeStatusRequest request)
-        {
-            return await RequestAsync <ChangeStatusRequest, ChangeStatusResponse>(request);
-        }
+            => await RequestAsync<ChangeStatusRequest, ChangeStatusResponse>(request);
+
+
+
+        public JoinRoomResponse JoinRoom(JoinRoomRequest request)
+            => Request<JoinRoomRequest, JoinRoomResponse>(request);
 
         public async Task<JoinRoomResponse> JoinRoomAsync(JoinRoomRequest request)
-        {
-            return await RequestAsync <JoinRoomRequest, JoinRoomResponse>(request);
-        }
+            => await RequestAsync<JoinRoomRequest, JoinRoomResponse>(request);
+
+
+
+        public LeaveRoomResponse LeaveRoom(LeaveRoomRequest request)
+            => Request<LeaveRoomRequest, LeaveRoomResponse>(request);
 
         public async Task<LeaveRoomResponse> LeaveRoomAsync(LeaveRoomRequest request)
-        {
-            return await RequestAsync <LeaveRoomRequest, LeaveRoomResponse>(request);
-        }
+            => await RequestAsync<LeaveRoomRequest, LeaveRoomResponse>(request);
+
+
+
+        public StartRoomResponse StartRoom(StartRoomRequest request)
+            => Request<StartRoomRequest, StartRoomResponse>(request);
 
         public async Task<StartRoomResponse> StartRoomAsync(StartRoomRequest request)
-        {
-            return await RequestAsync <StartRoomRequest, StartRoomResponse>(request);
-        }
+            => await RequestAsync<StartRoomRequest, StartRoomResponse>(request);
+
 
 
         //Puzzle
+        public AddPuzzlesResponse AddPuzzle(AddPuzzlesRequest request)
+            => Request<AddPuzzlesRequest, AddPuzzlesResponse>(request);
+
         public async Task<AddPuzzlesResponse> AddPuzzleAsync(AddPuzzlesRequest request)
-        {
-            return await RequestAsync<AddPuzzlesRequest, AddPuzzlesResponse>(request);
-        }
+            => await RequestAsync<AddPuzzlesRequest, AddPuzzlesResponse>(request);
 
         #endregion
 
