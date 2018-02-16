55a48cd7a825d4227c6a4dbbb31b2134d47b5f40
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2018-02-16 01:18:07 +0100

    Dodata bazna klasa

--- Projekat/PuzzleStorm/DTOLibrary/Requests/AddPuzzlesRequest.cs
+++ Projekat/PuzzleStorm/DTOLibrary/Requests/AddPuzzlesRequest.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DTOLibrary.Responses;
 
 namespace DTOLibrary.Requests
 {
-    public class AddPuzzlesRequest
+    public class AddPuzzlesRequest : PostLoginRequest
     {
         
     }
