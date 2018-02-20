b59ed2df5cd651396b3cb5f087d55453cf60fc11
Dacha204 <dalibor.aleksic.dacha@gmail.com>
2018-02-20 22:29:38 +0100

    Remove list fix

--- Projekat/PuzzleStorm/Client/ViewModel/RoomsListViewModel.cs
+++ Projekat/PuzzleStorm/Client/ViewModel/RoomsListViewModel.cs
@@ -100,8 +100,11 @@ namespace Client {
                     case RoomUpdateType.Deleted:
                     case RoomUpdateType.Started:
                     case RoomUpdateType.Filled:
-                        RoomsItemsList.Remove(RoomsItemsList.Single(x => x.RoomId == update.RoomId));
-                        ListRooms.Instance.RoomsItemsList = RoomsItemsList;
+                        if (RoomsItemsList.Any(x => x.RoomId == update.RoomId))
+                        {
+                            RoomsItemsList.Remove(RoomsItemsList.Single(x => x.RoomId == update.RoomId));
+                            ListRooms.Instance.RoomsItemsList = RoomsItemsList;
+                        }
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
